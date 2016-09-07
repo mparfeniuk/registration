e9c6455393dbd21149eabd8a444bb99ff0b4fdb4
Maksim Parfeniuk <mparfeniuk@gmail.com>
2016-09-07 14:52:41 +0300

    Creating basic shell for js part

---
+++ script.js
@@ -0,0 +1,32 @@
+var LK = {};
+
+LK.registration = (function ($) {
+
+    function init() {
+        // Global init
+    };
+
+    function checkPasswordMatch() {
+       // Function for compare passwords
+    };
+
+    function submitForm() {
+        // Submit listener
+    };
+
+    function sendRequest() {
+        // All magic with ajax request
+    };
+
+    return {
+        init: init
+    }
+
+})(jQuery);
+
+
+$(document).ready(function () {
+
+    // Initialization
+
+});
