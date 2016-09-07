a42ee4b4b26094b9bbec32af1bc3d27040aafce6
Maksim Parfeniuk <mparfeniuk@gmail.com>
2016-09-07 16:40:08 +0300

    Checking passwords match

--- script.js
+++ script.js
@@ -5,20 +5,47 @@ LK.registration = (function ($) {
         $birthdayField = $rgForm.find('#lk-birthday'),
         $passwordField = $rgForm.find('#lk-password'),
         $confirmPasswordField = $rgForm.find('#lk-confirm-password'),
-        $submitBtn = $rgForm.find('#lk-contact-submit');
+        $submitBtn = $rgForm.find('#lk-contact-submit'),
+        passwordMatch = false;
 
     function init() {
         if (!$rgForm) return;
 
         initDatePicker($birthdayField);
+
+        $passwordField.add($confirmPasswordField).on('keyup', function (e) {
+            checkPasswordMatch($passwordField, $confirmPasswordField)
+        });
+
+        $rgForm.on('submit', function (e) {
+            e.preventDefault();
+            submitForm();
+        });
     };
 
-    function checkPasswordMatch() {
-       // Function for compare passwords
+    function checkPasswordMatch(password, password_c) {
+        passwordMatch = (password.val() == password_c.val()) ? true : false;
+
+        if (!passwordMatch) {
+            password_c.css('borderColor', '#e8503c');
+        } else if (password.val() == '' && password_c.val() == '') {
+            password_c.css('borderColor', '#d8d8d8');
+        } else {
+            password_c.css('borderColor', '#64c7c7');
+            $('.lk-confirm-password-msg').html("");
+        }
     };
+
+    function notifier(id, type, msg) {
+        $(id).html("<span class='" + type + "'>" + msg + "</span>");
+    }
 
     function submitForm() {
-        // Submit listener
+        if (passwordMatch) {
+            console.log("Send request")
+        } else {
+            notifier(".lk-confirm-password-msg", "error", "Passwords don't match.")
+        }
     };
 
     function sendRequest() {
